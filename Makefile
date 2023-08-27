c6213d7e1d527d9ef9f6b541da077e5ece975756
salmanbarani <salman.barani@postpay.io>
2023-08-27 03:35:53 +0300

    connecting SDL with small bugs

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 INCLUDES= -I ./include
 FLAGS= -g # Debugging symbol
 all:
-	gcc ${FLAGS} ${INCLUDES} ./src/main.c -o ./bin/main
+	gcc ${FLAGS} ${INCLUDES} ./src/main.c -L ./lib -lSDL2 -o ./bin/main
--- src/main.c
+++ src/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include "SDL2/SDL.h"
+
 
 int main(int argc, char** argv) 
 {
-
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = SDL_CreateWindow(
+		"Chip8 Window",  		// Title of the window
+		SDL_WINDOWPOS_UNDEFINED,	// X position of the window 
+		SDL_WINDOWPOS_UNDEFINED,	// Y position of the window
+		640 , 320,			// Width and Heights of the window
+		SDL_WINDOW_SHOWN		// for the flag
+	);
+	
 	printf("Hello world!\n");
+	SDL_DestroyWindow(window);
 	return 0;
 }
