c9c4eae7dc2da83eebfdea7e365788324fc04bf2
salmanbarani <salmanAndB@outlook.com>
2023-09-05 06:29:01 +0300

    structuring the project

---
+++ include/chip8.h
@@ -0,0 +1,12 @@
+#ifndef CHIP8_H // part of the header file
+#define CHIP8_H // part of the header file
+
+// Everyting related to chip8 goes inside the struct like memory, keyboard, and so on
+struct chip8
+{
+
+
+}
+
+
+#endif // part of the header file
